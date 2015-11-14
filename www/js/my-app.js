fad8afb827924899b8f88f9135b21b7238840e1d
hassanomnix <hassan.a@omnix.ae>
2015-11-14 20:45:07 +0200

    koko

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -10,6 +10,14 @@ var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
 });
 
+//We can also add callback for all pages:
+myApp.onPageInit('*', function (page) {
+    console.log(page.name + ' initialized');
+});
+
+
+
+
 // Callbacks to run specific code for specific pages, for example for About page:
 myApp.onPageInit('about', function (page) {
     // run createContentPage func after link was clicked
@@ -20,8 +28,9 @@ myApp.onPageInit('about', function (page) {
 
 // Generate dynamic page
 var dynamicPageIndex = 0;
+
 function createContentPage() {
-	mainView.router.loadContent(
+    mainView.router.loadContent(
         '<!-- Top Navbar-->' +
         '<div class="navbar">' +
         '  <div class="navbar-inner">' +
@@ -44,5 +53,97 @@ function createContentPage() {
         '  </div>' +
         '</div>'
     );
-	return;
+    return;
+}
+
+
+
+
+var pictureSource; // picture source
+var destinationType; // sets the format of returned value 
+// Wait for PhoneGap to connect with the device
+//
+var config = {
+   enableHighAccuracy: true,
+   timeout: 20000,
+   maximumAge: 18000000
+}
+
+document.addEventListener("deviceready", onDeviceReady, false);
+// PhoneGap is ready to be used!
+//
+function onDeviceReady() {
+    pictureSource = navigator.camera.PictureSourceType;
+    destinationType = navigator.camera.DestinationType;
+    console.log('navigator: ' , navigator);
+    //navigator.geolocation.getCurrentPosition(maponSuccess, maponError,config);
+    navigator.geolocation.getCurrentPosition(maponSuccess, maponError,  {enableHighAccuracy: true,maximumAge:60000, timeout: 5000});
+}
+// Called when a photo is successfully retrieved
+//
+
+function maponSuccess(position){
+     alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+    console.log(position);
+    var longitude = position.coords.longitude;
+    var latitude = position.coords.latitude;
+    var latLong = new google.maps.LatLng(latitude, longitude);
+
+    var mapOptions = {
+        center: latLong,
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    var marker = new google.maps.Marker({
+          position: latLong,
+          map: map,
+          title: 'my location'
+      });
+}
+
+function maponError(error){
+    alert("the code is " + error.code + ". \n" + "message: " + error.message);
+}
+
+function onPhotoFileSuccess(imageData) {
+    // Get image handle
+    //console.log(JSON.stringify(imageData));
+
+    // Get image handle
+    //
+    var largeImage = document.getElementById('largeImage');
+    // Unhide image elements
+    //
+    largeImage.style.display = 'block';
+    // Show the captured photo
+    // The inline CSS rules are used to resize the image
+    //
+    largeImage.src = imageData;
+}
+
+
+function capturePhotoWithFile() {
+    navigator.camera.getPicture(onPhotoFileSuccess, onFail, {
+        quality: 75,
+        encodingType: Camera.EncodingType.JPEG,
+        destinationType: Camera.DestinationType.FILE_URI,
+        correctOrientation: true
+    });
+}
+
+
+// Called if something bad happens.
+// 
+function onFail(message) {
+    //    alert('Failed because: ' + message);
 }
