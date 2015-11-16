b2b220ee26f7187e1bdb91d4dd397297d6013190
hassanomnix <hassan.a@omnix.ae>
2015-11-16 10:29:42 +0200

    123

--- platforms/android/assets/www/js/my-app.js
+++ platforms/android/assets/www/js/my-app.js
@@ -1,72 +1,109 @@
 // Initialize your app
-var myApp = new Framework7();
+var myApp = new Framework7({
+    animateNavBackIcon: true,
+    // Enable templates auto precompilation
+    precompileTemplates: true,
+    // Enabled pages rendering using Template7
+    swipeBackPage: true,
+    pushState: true,
+    template7Pages: true
+});
 
 // Export selectors engine
 var $$ = Dom7;
+var pictureSource; // picture source
+var destinationType;
 
-// Add view
+// Add main View
 var mainView = myApp.addView('.view-main', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
-});
-
-//We can also add callback for all pages:
-myApp.onPageInit('*', function (page) {
-    console.log(page.name + ' initialized');
+    // Enable dynamic Navbar
+    dynamicNavbar: false,
 });
 
 
+function save() {
 
-
-// Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
+    $('input').each(function () {
+        localStorage.setItem($(this).attr('name'), $(this).val());
     });
-});
-
-// Generate dynamic page
-var dynamicPageIndex = 0;
-
-function createContentPage() {
-    mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-    return;
 }
 
 
+function CameraAccedantHits() {
+
+}
+
+function maponSuccess(position) {
+
+    var longitude = position.coords.longitude;
+    var latitude = position.coords.latitude;
+
+}
+
+function maponError(error) {
+    alert("the code is " + error.code + ". \n" + "message: " + error.message);
+}
+
+function onPhotoURISuccessfarimage(imageData) {
+
+    $('.farimage').attr({
+        'src': imageData,
+        'width': '100%'
+    });
+}
+
+function onPhotoURISuccesshitsimages(imageData) {
+
+    
+    var image = '<li><a rel="gallery-3" href="'+imageData+'" title="Photo title" class="swipebox"><img width="100%" height="80%" src="'+imageData+'" alt="image"/></a></li>';
+    $('#photoslist').prepend(image);
+
+}
 
 
-var pictureSource; // picture source
-var destinationType; // sets the format of returned value 
-// Wait for PhoneGap to connect with the device
-//
-var config = {
-   enableHighAccuracy: true,
-   timeout: 20000,
-   maximumAge: 18000000
+function capturePhotoWithFile(type) {
+
+    if (type == 'farimage') {
+
+        navigator.camera.getPicture(onPhotoURISuccessfarimage, onFail, {
+            quality: 50,
+            encodingType: Camera.EncodingType.JPEG,
+            destinationType: Camera.DestinationType.FILE_URI,
+            correctOrientation: true
+        });
+    } else {
+        navigator.camera.getPicture(onPhotoURISuccesshitsimages, onFail, {
+            quality: 50,
+            encodingType: Camera.EncodingType.JPEG,
+            destinationType: Camera.DestinationType.FILE_URI,
+            correctOrientation: true
+        });
+    }
+}
+
+function getPhoto(source, type) {
+    // Retrieve image file location from specified source
+
+
+    if (type == 'farimage') {
+        navigator.camera.getPicture(onPhotoURISuccessfarimage, onFail, {
+            quality: 50,
+            destinationType: destinationType.FILE_URI,
+            sourceType: source
+        });
+    } else {
+        navigator.camera.getPicture(onPhotoURISuccesshitsimages, onFail, {
+            quality: 50,
+            destinationType: destinationType.FILE_URI,
+            sourceType: source
+        });
+    }
+
+}
+// Called if something bad happens.
+// 
+function onFail(message) {
+    //    alert('Failed because: ' + message);
 }
 
 document.addEventListener("deviceready", onDeviceReady, false);
@@ -75,75 +112,164 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     pictureSource = navigator.camera.PictureSourceType;
     destinationType = navigator.camera.DestinationType;
-    console.log('navigator: ' , navigator);
-    //navigator.geolocation.getCurrentPosition(maponSuccess, maponError,config);
-    navigator.geolocation.getCurrentPosition(maponSuccess, maponError,  {enableHighAccuracy: true,maximumAge:60000, timeout: 5000});
+
+    //navigator.geolocation.getCurrentPosition(maponSuccess, maponError,  {enableHighAccuracy: true,maximumAge:60000, timeout: 5000});
 }
-// Called when a photo is successfully retrieved
-//
 
-function maponSuccess(position){
-     alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
-    console.log(position);
-    var longitude = position.coords.longitude;
-    var latitude = position.coords.latitude;
-    var latLong = new google.maps.LatLng(latitude, longitude);
+$$(document).on('pageInit', function (e) {
 
-    var mapOptions = {
-        center: latLong,
-        zoom: 13,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+
+    /* Custom Code */
+var calendarDefault = myApp.calendar({
+    input: '#calendar-default',
+});  
+
+    var carscounter = 2;
+
+    $('a.addcar').click(function () {
+
+        carscounter++;
+        var element = '<li><div class="item-content"><div class="item-inner"><a href="#" class="removecar" data-remove="' + carscounter + '"><i class="fa fa-times-circle"></i></a>&nbsp;<div class="item-title label">رقم المركبة ' + carscounter + '</div><div class="item-input"><input type="text" name="other_car_number_' + carscounter + '" id="other_car_number"/></div></div></div></li>';
+
+        $('ul.car_numbers_forms').append(element);
+
+
+    });
+
+
+
+    /* end Custom Code */
+
+
+    $(".swipebox").swipebox();
+    $(".videocontainer").fitVids();
+
+    $("#ContactForm").validate({
+        submitHandler: function (form) {
+            ajaxContact(form);
+            return false;
+        }
+    });
+
+
+    $(".posts li").hide();
+    size_li = $(".posts li").size();
+    x = 3;
+    $('.posts li:lt(' + x + ')').show();
+    $('#loadMore').click(function () {
+        x = (x + 1 <= size_li) ? x + 1 : size_li;
+        $('.posts li:lt(' + x + ')').show();
+        if (x == size_li) {
+            $('#loadMore').hide();
+            $('#showLess').show();
+        }
+    });
+
+
+
+    $("a.switcher").bind("click", function (e) {
+        e.preventDefault();
+
+        var theid = $(this).attr("id");
+        var theproducts = $("ul#photoslist");
+        var classNames = $(this).attr('class').split(' ');
+
+
+        if ($(this).hasClass("active")) {
+            // if currently clicked button has the active class
+            // then we do nothing!
+            return false;
+        } else {
+            // otherwise we are clicking on the inactive button
+            // and in the process of switching views!
+
+            if (theid == "view13") {
+                $(this).addClass("active");
+                $("#view11").removeClass("active");
+                $("#view11").children("img").attr("src", "images/switch_11.png");
+
+                $("#view12").removeClass("active");
+                $("#view12").children("img").attr("src", "images/switch_12.png");
+
+                var theimg = $(this).children("img");
+                theimg.attr("src", "images/switch_13_active.png");
+
+                // remove the list class and change to grid
+                theproducts.removeClass("photo_gallery_11");
+                theproducts.removeClass("photo_gallery_12");
+                theproducts.addClass("photo_gallery_13");
+
+            } else if (theid == "view12") {
+                $(this).addClass("active");
+                $("#view11").removeClass("active");
+                $("#view11").children("img").attr("src", "images/switch_11.png");
+
+                $("#view13").removeClass("active");
+                $("#view13").children("img").attr("src", "images/switch_13.png");
+
+                var theimg = $(this).children("img");
+                theimg.attr("src", "images/switch_12_active.png");
+
+                // remove the list class and change to grid
+                theproducts.removeClass("photo_gallery_11");
+                theproducts.removeClass("photo_gallery_13");
+                theproducts.addClass("photo_gallery_12");
+
+            } else if (theid == "view11") {
+                $("#view12").removeClass("active");
+                $("#view12").children("img").attr("src", "images/switch_12.png");
+
+                $("#view13").removeClass("active");
+                $("#view13").children("img").attr("src", "images/switch_13.png");
+
+                var theimg = $(this).children("img");
+                theimg.attr("src", "images/switch_11_active.png");
+
+                // remove the list class and change to grid
+                theproducts.removeClass("photo_gallery_12");
+                theproducts.removeClass("photo_gallery_13");
+                theproducts.addClass("photo_gallery_11");
+
+            }
+
+        }
+
+    });
+
+    document.addEventListener('touchmove', function (event) {
+        if (event.target.parentNode.className.indexOf('navbarpages') != -1 || event.target.className.indexOf('navbarpages') != -1) {
+            event.preventDefault();
+        }
+    }, false);
+
+    // Add ScrollFix
+    var scrollingContent = document.getElementById("pages_maincontent");
+    new ScrollFix(scrollingContent);
+
+
+    var ScrollFix = function (elem) {
+        // Variables to track inputs
+        var startY = startTopScroll = deltaY = undefined,
+
+            elem = elem || elem.querySelector(elem);
+
+        // If there is no element, then do nothing	
+        if (!elem)
+            return;
+
+        // Handle the start of interactions
+        elem.addEventListener('touchstart', function (event) {
+            startY = event.touches[0].pageY;
+            startTopScroll = elem.scrollTop;
+
+            if (startTopScroll <= 0)
+                elem.scrollTop = 1;
+
+            if (startTopScroll + elem.offsetHeight >= elem.scrollHeight)
+                elem.scrollTop = elem.scrollHeight - elem.offsetHeight - 1;
+        }, false);
     };
 
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-    var marker = new google.maps.Marker({
-          position: latLong,
-          map: map,
-          title: 'my location'
-      });
-}
-
-function maponError(error){
-    alert("the code is " + error.code + ". \n" + "message: " + error.message);
-}
-
-function onPhotoFileSuccess(imageData) {
-    // Get image handle
-    //console.log(JSON.stringify(imageData));
-
-    // Get image handle
-    //
-    var largeImage = document.getElementById('largeImage');
-    // Unhide image elements
-    //
-    largeImage.style.display = 'block';
-    // Show the captured photo
-    // The inline CSS rules are used to resize the image
-    //
-    largeImage.src = imageData;
-}
 
 
-function capturePhotoWithFile() {
-    navigator.camera.getPicture(onPhotoFileSuccess, onFail, {
-        quality: 75,
-        encodingType: Camera.EncodingType.JPEG,
-        destinationType: Camera.DestinationType.FILE_URI,
-        correctOrientation: true
-    });
-}
-
-
-// Called if something bad happens.
-// 
-function onFail(message) {
-    //    alert('Failed because: ' + message);
-}
+});
